272902d9f8be04d8ca943137768a4f26def3e4f4
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-04-25 22:15:41 +0530

    Reactored: patterns.js

--- patterns.js
+++ patterns.js
@@ -1,35 +1,73 @@
-const style = function(symbol, times) {
-  return symbol.repeat(times);
+const stars = function(width) {
+  return "*".repeat(width);
 }
 
-const spaces = function(times) {
-  return style(' ', times);
+const spaces = function(width) {
+  return " ".repeat(width);
 }
 
-const stars = function(times) {
-  return style('*', times);
-}
-
-const hollowLines = function(width) {
+const hollowLine = function(width) {
   return stars(1) + spaces(width - 2) + stars(1);
 }
 
-const applyStyle = function(style, lineWidths) {
-  return lineWidths.map(style);
+const applyStyle = function(style, widths) {
+  return widths.map(style);
 }
 
-const leftPad = function(maxWidth, lineWidth) {
+const filled = function() {
+  return [stars];
 }
 
-const hollow = function() {
-  return [stars, hollowLines, stars];
+const hollow = function(list) {
+  const group = [];
+  group.push([list[0]]);
+  group.push(list.slice(1, -1));
+  group.push(list.slice(-1));
+
+  return group;
 }
 
-const renderStyleGroup = function(styles, lineWidthGroups) {
+const rectangle = function(row, col) {
+  const widths = [];
+
+  for(let index = 0; index < row; index++) {
+    widths.push(col);
+  }
+
+  return widths;
+}
+
+const monolith = function(lineWidths) {
+  return [lineWidths];
+}
+
+const getShapeData = function(shape, width, breadth) {
+  const shapes = {
+    hollowRectangle: {
+      styles: [stars, hollowLine, stars],
+      lineWidthGroups: hollow(rectangle(width, breadth))
+    },
+    rectangle: {
+      styles: [stars],
+      lineWidthGroups: monolith(rectangle(width, breadth))
+    }
+  }
+
+  return shapes[shape];
+}
+
+const generatePattern = function(pattern, width, breadth) {
+  const shape = getShapeData(pattern, width, breadth);
+  const styles = shape.styles;
+  const lineWidthGroups = shape.lineWidthGroups;
+
   return styles.flatMap(function(style, index) {
     return applyStyle(style, lineWidthGroups[index]);
   });
 }
 
-console.log(renderStyleGroup([stars, spaces, stars], [[1],  [2, 3, 4, 5], [6]]));
+let shape = generatePattern('hollowRectangle', 5, 4);
+console.log(shape.join('\n'));
 
+shape = generatePattern('rectangle', 5, 4);
+console.log(shape.join('\n'));
